f7a4686328d37bfac4da1f4f96d7a874d48c3482
markyang <ideamark@qq.com>
2023-09-17 21:36:15 +0800

    modified:   run.py 	modified:   video2audio.py 	modified:   words.csv

--- run.py
+++ run.py
@@ -1,16 +1,17 @@
+import os
 import csv
 
-from settings import output_folder, csv_path
-from split_video import split_video, clear_folder
+from settings import csv_path, output_folder
+from split_video import split_video
 
 
-clear_folder(output_folder)
 with open(csv_path, 'r') as fp:
     rows = csv.reader(fp)
     for row in rows:
         word = row[0]
-        paraphrase = row[1]
-        try:
-            split_video(word, paraphrase)
-        except Exception:
-            pass
+        if word not in os.listdir(output_folder):
+            paraphrase = row[1]
+            try:
+                split_video(word, paraphrase)
+            except Exception as e:
+                print(e)
